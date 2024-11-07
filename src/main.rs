8a89e399c4a030ce908f2b0c23cf22bef49648e8
u1F98E <u1f98e@proton.me>
2024-11-06 23:40:48 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,115 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read, Seek, SeekFrom, Write};
+use std::path::PathBuf;
+
+use rand::Rng;
+use termcolor::{ColorSpec, StandardStream, WriteColor};
+
+struct LineSeeker<R: Read + Seek> {
+    reader: BufReader<R>,
+    positions: Vec<usize>,
+}
+
+impl<R> LineSeeker<R>
+where
+    R: Read + Seek,
+{
+    pub fn new(read: R) -> io::Result<LineSeeker<R>> {
+        let mut reader = BufReader::new(read);
+        let mut positions = Vec::new();
+        let mut current_pos = 0;
+        let mut _buf = Vec::new();
+        loop {
+            let count = reader.read_until(b'\n', &mut _buf)?;
+            if count == 0 {
+                break;
+            }
+            positions.push(current_pos);
+            current_pos += count;
+        }
+
+        Ok(LineSeeker { reader, positions })
+    }
+
+    pub fn count(&self) -> usize {
+        self.positions.len()
+    }
+
+    pub fn get_line(&mut self, index: usize) -> io::Result<String> {
+        if self.positions.is_empty() {
+            return Ok(String::new());
+        }
+
+        let pos = self
+            .positions
+            .get(index)
+            .expect("index for line should be in range");
+        self.reader.seek(SeekFrom::Start(*pos as u64))?;
+
+        let mut buf = Vec::new();
+        self.reader.read_until(b'\n', &mut buf)?;
+        Ok(String::from_utf8(buf).expect(&format!("line {index} is not a valid utf8 string")))
+    }
+}
+
+fn msg_file_path() -> PathBuf {
+    std::env::var("MOTD_FILE")
+        .map(|p| PathBuf::from(p))
+        .unwrap_or_else(|_| {
+            PathBuf::from(dirs::config_local_dir().unwrap_or_default()).join("motd.conf")
+        })
+}
+
+fn hsl_to_rgb(h: f32, s: f32, l: f32) -> (u8, u8, u8) {
+    let chroma = (1. - f32::abs(2. * l - 1.)) * s;
+    let h_prime = h * 6.; // H' = H / 60deg
+    let x = chroma * (1. - f32::abs(f32::rem_euclid(h_prime, 2.) - 1.)); // X = C * (1 - |H' mod 2 - 1|)
+    let (r1, g1, b1) = if 0. <= h_prime && h_prime < 1. {
+        (chroma, x, 0.)
+    } else if h_prime < 2. {
+        (x, chroma, 0.)
+    } else if h_prime < 3. {
+        (0., chroma, x)
+    } else if h_prime < 4. {
+        (0., x, chroma)
+    } else if h_prime < 5. {
+        (x, 0., chroma)
+    } else {
+        (chroma, 0., x)
+    };
+
+    let m = l - (chroma / 2.);
+    let r = (r1 + m) * 255.;
+    let g = (g1 + m) * 255.;
+    let b = (b1 + m) * 255.;
+    (r as u8, g as u8, b as u8)
+}
+
+/// Returns a [termcolor::Color] with a random hue, full saturation, and a lightness
+/// between the provided `lightness_lower` and `lightness_upper` bounds (minimum 0.0, maximum 1.0)
+fn random_color(lightness_lower: f32, lightness_upper: f32) -> termcolor::Color {
+    let mut rng = rand::thread_rng();
+    let (r, g, b) = hsl_to_rgb(
+        rng.gen_range(0.0..1.0),
+        1.0,
+        rng.gen_range(lightness_lower..lightness_upper),
+    );
+    termcolor::Color::Rgb(r, g, b)
+}
+
+fn main() -> io::Result<()> {
+    let msg_file = File::open(msg_file_path()).unwrap_or_else(|e| {
+        eprintln!("motd: failed to open message file: {e}");
+        std::process::exit(1);
+    });
+
+    let mut lines = LineSeeker::new(msg_file).unwrap();
+    let index = rand::thread_rng().gen_range(0..lines.count());
+    let msg = lines.get_line(index)?;
+
+    let mut stdout = StandardStream::stdout(termcolor::ColorChoice::Auto);
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(random_color(0.5, 0.9))));
+    let _ = writeln!(&mut stdout, "{}", msg.trim());
+
+    Ok(())
+}
